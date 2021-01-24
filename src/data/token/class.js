d850ce790502b1b01b8ffac2885df57dfdb45c56
urban070 <53049686+urban070@users.noreply.github.com>
2021-01-24 23:56:50 +0100

    Fixed TVL and adjusted estimated rewards

--- src/data/token/class.js
+++ src/data/token/class.js
@@ -43,9 +43,10 @@ export default class Token {
       let p = wB / xB; // Price in ETH
 	  
       if (testnet) {
-	    if(x === 0) {
+	    if(x == 0) {
 		  p = 0.00023927;
 	    }
+	  console.log(x);
         this.price = p * 1250;
       } else {
         let i = await wethContract.methods.balanceOf(USDCWETHAddress).call();
@@ -121,11 +122,11 @@ export default class Token {
       let rewardRate = await stakeContract.methods.rewardRate().call();
       let userStaked = await stakeContract.methods.balanceOf(w3.address).call();
       let totalStaked = await this.contract.methods.balanceOf(stakeAddress).call();
-    userStaked = await w3.getWeiToETH(userStaked);
-    totalStaked = await w3.getWeiToETH(totalStaked);
-    BigNumber.config({ DECIMAL_PLACES: 18 });
-    let e = new BigNumber(userStaked / totalStaked * rewardRate * 60 * 60 * 24);
-      this.estimated = w3.getWeiToETH(e.toString(10));
+      userStaked = await w3.getWeiToETH(userStaked);
+      totalStaked = await w3.getWeiToETH(totalStaked);
+      rewardRate = await w3.getWeiToETH(rewardRate);
+      let e = new BigNumber(userStaked / totalStaked * rewardRate * 60 * 60 * 24);
+      this.estimated = e.toString(10);
     }
   }
 }
